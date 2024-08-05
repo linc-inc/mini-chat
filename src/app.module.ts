a92f4a9cebf94e9f9ebe9f0bac7a964d6ecd0cad
SantiagoGuerra19 <santiagoguerra@linclearning.com>
2024-08-05 16:22:46 -0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
